8efe6d729495e128c09a137ec169971cfdba3f18
Benjamin Vialle <benjaminvialle@gmail.com>
2010-10-18 08:55:58 +0200

    Adding reset_admin_demo script for Git

--- shell-scripts/reset_admin_demo.sh
+++ shell-scripts/reset_admin_demo.sh
@@ -3,12 +3,16 @@
 
 # short script to reset the MarkUs app available at http://www.markusproject.org/admin-demo
 
-export PATH=/bin 		# set path
+# set environment
+PATH="/bin:/usr/bin:/var/lib/gems/1.8/bin"
+#GEM_PATH="/home/markuspr/ruby/gems"
+export PATH
 export RAILS_ENV="production" 	# we want the prod-system reset
 
 LOGFILE="log/reset_admin_demo_db.log"
 
 cd /home/markus/sandbox/markus_trunk
-/usr/bin/rake repos:drop >> $LOGFILE 2>&1
-/usr/bin/rake db:reset RAILS_ENV=production > $LOGFILE 2>&1
-/usr/bin/rake markus:usability_test_setup RAILS_ENV=production > $LOGFILE 2>&1
+git pull origin master >> $LOGFILE 2>&1
+rake repos:drop > $LOGFILE 2>&1
+rake db:reset RAILS_ENV=production > $LOGFILE 2>&1
+rake markus:usability_test_setup RAILS_ENV=production > $LOGFILE 2>&1
